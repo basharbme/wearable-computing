0482eb8d33b574c2c28cdfec1f83d3d7943f08d9
Kobus Esterhuysen <kobus78@gmail.com>
2016-06-29 05:56:06 -0700

    final

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,15 @@
+################################################################################
+## This script does the following:
+## 1. Merges the training and the test sets to create one data set.
+## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+## 3. Uses descriptive activity names to name the activities in the data set
+## 4. Appropriately labels the data set with descriptive variable names.
+## 5. From the data set in step 4, creates a second, independent tidy data set 
+##    with the average of each variable for each activity and each subject.
+################################################################################
 
-# Download and unzip
+## Download and unzip
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-
 temp <- tempfile()
 download.file(fileUrl, destfile = "temp", method = "curl")
 unzip("temp", exdir = ".")
@@ -9,22 +17,8 @@ list.files("./")
 unlink("temp")
 list.files("./")
 
-## TRAIN
-# Inertial Signals
-#body_acc_x_train <- read.table("./UCI HAR Dataset/train/Inertial Signals/body_acc_x_train.txt", sep = "")
-#length(names(body_acc_x_train))
-
-#body_gyro_x_train <- read.table("./UCI HAR Dataset/train/Inertial Signals/body_gyro_x_train.txt", sep = "")
-#length(names(body_gyro_x_train))
-
-#total_acc_x_train <- read.table("./UCI HAR Dataset/train/Inertial Signals/total_acc_x_train.txt", sep = "")
-#length(names(total_acc_x_train))
-
-#9*128
-#[1] 1152
-
 ########################################################################
-### Merges the training and the test sets to create one data set. ######
+### 1. Merge the training and the test sets to create one data set. ####
 ########################################################################
 # read names of features
 features <- read.table("./UCI HAR Dataset/features.txt", sep = "")
@@ -32,60 +26,31 @@ features <- tbl_df(features)
 
 # read train data
 X_train <- read.table("./UCI HAR Dataset/train/X_train.txt", sep = "")
-#X_train <- tbl_df(X_train)
-dim(X_train)
-
-#
-#subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt", sep = "")
-#dim(subject_train)
-
-#y_train <- read.table("./UCI HAR Dataset/train/y_train.txt", sep = "")
-#dim(y_train)
-#
-
-#X_train_y <- bind_cols(X_train, y_train)
-#X_train_y <- cbind(X_train, subject_train, y_train)
-#dim(X_train_y)
-
 
 # read test data
 X_test <- read.table("./UCI HAR Dataset/test/X_test.txt", sep = "")
-#X_test <- tbl_df(X_test)
-dim(X_test)
-
-#
-#subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt", sep = "")
-#dim(subject_test)
-
-#y_test <- read.table("./UCI HAR Dataset/test/y_test.txt", sep = "")
-#dim(y_test)
-
-#X_test_y <- bind_cols(X_test, y_test)
-#X_test_y <- cbind(X_test, subject_test, y_test)
-#dim(X_test_y)
 
 # combine train and test data
-#X <- bind_rows(X_train_y, X_test_y)
-#X <- rbind(X_train_y, X_test_y)
 X <- rbind(X_train, X_test)
 dim(X)
 
 X2 <- tbl_df(X)
 
-# descriptive variable names
-#names(X2) <- c(as.character(features$V2), "Subject", "Activity")
+
+########################################################################
+### 4. Label the data set with descriptive variable names ##############
+########################################################################
 names(X2) <- c(as.character(features$V2))
 length(names(X2))
 
 
 ###############################################################################################
-### Extracts only the measurements on the mean and standard deviation for each measurement. ###
+### 2. Extract only the measurements on the mean and standard deviation for each measurement. #
 ###############################################################################################
-#X2 %>% select(contains(mean))
 X3 <- X2[, grepl("mean|std", colnames(X2))]
 
 
-
+# add Activity and Subject
 subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt", sep = "")
 y_train <- read.table("./UCI HAR Dataset/train/y_train.txt", sep = "")
 X4_train <- cbind(subject_train, y_train)
@@ -96,8 +61,6 @@ X4_test <- cbind(subject_test, y_test)
 
 # combine train and test data
 X4 <- rbind(X4_train, X4_test)
-
-    
 X5 <- cbind(X3, X4)
 
 len <- length(names(X5))
@@ -105,14 +68,18 @@ names(X5)[len-1] <- "Subject"
 names(X5)[len] <- "Activity"
 names(X5)
 
-# descriptive activity names
+
+################################################################################
+### 3. Use descriptive activity names to name the activities in the data set ###
+################################################################################
 X5$Activity <- factor(X5$Activity, labels = c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS", "SITTING", "STANDING", "LAYING"))
-#X5$Subject <- factor(X5$Subject)
 
+
+####################################################################################################
+### 5. Create a tidy data set with the average of each variable for each activity and each subject #
+####################################################################################################
 X6 <- tbl_df(X5)
-
 X7 <- group_by(X6, Activity, Subject)
-#X8 <- summarise(X7, mean(X7$"tBodyAcc-mean()-X"))
 X8 <- summarise_each(X7, funs(mean))
 
-
+write.table(X8, file = "tidy.txt", row.names = FALSE)
